6ac295b54c0d255cdfd8bcaac1d35992af31d220
aiteginbaratov <redmifly@gmail.com>
2024-07-24 11:45:02 +0600

    Изменил ветку main1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Cоздание первой ветки");
+        System.out.println("Клонирование проета");
+        System.out.println("Изменеие первой ветки");
     }
 }
